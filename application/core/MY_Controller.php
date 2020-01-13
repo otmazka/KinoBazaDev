2e68b3a1419f074a39c612edbb4be18d0cdea414
otmazka <svetlankas@hot.ee>
2020-01-13 19:41:41 +0200

    13.01.2020

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller {
+public function __construct(){
+	parent::__construct();
+
+	$this->data['title'] ="Кинобаза - сайт о кино";
+	$this->load->model('News_model');
+	$this->data['news'] = $this->News_model->getNews();
+	$this->load->model('Films_model');
+	$this->data['movie'] = $this->Films_model->getFilmsByRating(10);
+	$this->data['category'] ="";
+}
+}
