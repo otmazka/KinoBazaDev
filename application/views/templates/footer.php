24f6181356a9ad949093d14486d2e2d04ba085fd
Viktoria Bugaeva <Viktoria Bugaeva vikyvv@mail.ru>
2020-01-24 18:13:02 +0200

    templates footer, new view

---
+++ application/views/templates/footer.php
@@ -0,0 +1,22 @@
+<!--Footer start-->
+</div>
+<?php $this->load->view('templates/menu', $news, $films); ?>
+
+</div>
+</div>
+<div class="clear"></div>
+</div>
+
+<footer>
+	<div class="container">
+		<p class="text-center"> <a href="https://kinopoisk.com">Kinopoisk.com</a> </p>
+	</div>
+</footer>
+<!--jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+<script src="https://ajax/libs/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
+<!-- Footer end -->
